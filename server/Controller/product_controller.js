9f31c8830a98759989ec645fc5fc40d959e97c82
Fujail Akram <fujail5july@gmail.com>
2026-07-06 17:26:31 +0530

    Developed product api

--- server/Controller/product_controller.js
+++ server/Controller/product_controller.js
@@ -12,6 +12,22 @@ export const get_products = async (req, res) => {
   }
 };
 
+export const get_product = async (req, res) => {
+  try {
+    const product_id = req.params.id;
+    const product = await prisma.product.findFirst({
+      where: {
+        id: Number(product_id)
+      }
+    })
+    return res.status(200).json({ success: true, data: product });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+}
+
 export const create_product = async (req, res) => {
   try {
     const { name, description, sku, basePrice, isVariable, categoryId } =
@@ -37,3 +53,48 @@ export const create_product = async (req, res) => {
       .json({ success: false, message: "Internal server error" });
   }
 };
+
+export const update_product = async (req, res) => {
+  try {
+    const product_id = req.params.id;
+    const { name, description, sku, basePrice, isVariable, categoryId } =
+      req.body;
+    const updated_product = await prisma.product.update({
+      where: {
+        id: Number(product_id)
+      },
+      data: {
+        categoryId: Number(categoryId),
+        name,
+        description,
+        sku,
+        basePrice: new Prisma.Decimal(basePrice),
+        isVariable,
+      },
+    });
+    return res.status(201).json({
+      success: true,
+      data: update_product,
+      message: "Product updated",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+
+export const delete_product = async (req, res) => {
+  try {
+    const product_id = req.params.id;
+    await prisma.product.delete({
+      where: {
+        id: Number(product_id)
+      }
+    })
+    return res.status(200).json({ success: true, message: `Product with id ${product_id} has been deleted` })
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Internal server error" })
+  }
+}
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import {
   create_product,
+  delete_product,
+  get_product,
   get_products,
+  update_product,
 } from "../Controller/product_controller.js";
 const router = Router();
 
 router.get("/", get_products);
+router.get("/:id", get_product)
 router.post("/", create_product);
+router.patch("/:id", update_product)
+router.delete("/:id", delete_product)
 
 export default router;
